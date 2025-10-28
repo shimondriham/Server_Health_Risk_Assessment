9f8cc749e917609700b7553d927b909b247210b9
shimon driham <95618903+shimondriham@users.noreply.github.com>
2025-10-28 23:20:38 +0200

    start

--- db/mongoConnect.js
+++ db/mongoConnect.js
@@ -6,8 +6,7 @@ async function main() {
   mongoose.set("strictQuery", false);
   await mongoose.connect(
     // Your connection strings 
-    // `mongodb+srv://${process.env.USER_DB}:${process.env.PASS_DB}@cluster0.frzne.mongodb.net/Functional_Movement_Screening`
     `mongodb+srv://${process.env.USER_DB}:${process.env.PASS_DB}@cluster0.frzne.mongodb.net/Health_Risk_Assessment`
   );
-  console.log("mongo connect frome start_node");
+  console.log("mongo connect frome Health_Risk_Assessment");
 }
