b49f92eb8fe219c7027c0f715ebe2a40dbd5c0c1
shimon driham <95618903+shimondriham@users.noreply.github.com>
2025-10-28 23:18:56 +0200

    start

---
+++ db/mongoConnect.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+main().catch((err) => console.log(err));
+
+async function main() {
+  mongoose.set("strictQuery", false);
+  await mongoose.connect(
+    // Your connection strings 
+    // `mongodb+srv://${process.env.USER_DB}:${process.env.PASS_DB}@cluster0.frzne.mongodb.net/Functional_Movement_Screening`
+    `mongodb+srv://${process.env.USER_DB}:${process.env.PASS_DB}@cluster0.frzne.mongodb.net/Health_Risk_Assessment`
+  );
+  console.log("mongo connect frome start_node");
+}
---
+++ models/userModel.js
@@ -0,0 +1,42 @@
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const Joi = require("joi");
+
+const userSchema = new mongoose.Schema({
+    fullName: String,
+    email: String,
+    password: String,
+    role: {
+        type: String, default: "user"
+    },
+    verifictionCode: String,
+    verifiction: {
+        type: Boolean, default: false
+    },
+});
+
+exports.UserModel = mongoose.model("users", userSchema);
+
+exports.validUser = (_bodyData) => {
+
+    let joiSchema = Joi.object({
+        fullName: Joi.string().min(2).max(99).required(),
+        email: Joi.string().min(5).max(99).email().required(),
+        password: Joi.string().min(2).max(99).required(),
+    });
+
+    return joiSchema.validate(_bodyData);
+};
+
+exports.validateLogin = (_bodyReq) => {
+    let joiSchema = Joi.object({
+        email: Joi.string().min(2).max(150).email().required(),
+        password: Joi.string().min(3).max(100).required(),
+    })
+    return joiSchema.validate(_bodyReq);
+}
+
+exports.genToken = (_userId, _role) => {
+    let token = jwt.sign({ _id: _userId, role: _role }, process.env.JWT_SECRET, { expiresIn: "1440mins" });
+    return token;
+}
